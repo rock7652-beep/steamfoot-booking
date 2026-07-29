d69625b4958e179626803dbd6b425c2126c33dc5
rock7652-beep <rock7652@gmail.com>
2026-07-29 18:48:53 +0800

    fix: remove conflicting booking route

--- src/app/book/zhubei/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-const ZHUBEI_TRIAL_BOOKING_URL =
-  "https://www.steamfoot.com/pricing/experience/zhubei/book#booking-form";
-
-export function GET(): NextResponse {
-  return NextResponse.redirect(ZHUBEI_TRIAL_BOOKING_URL, 307);
-}
